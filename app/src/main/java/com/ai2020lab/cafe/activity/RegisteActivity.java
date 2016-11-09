f44937debd116b25787e1b35fc42e1b8f7c1ca78
Rocky <liliang9@asiainfo.com>
2016-11-09 15:42:01 +0800

    增加登录界面

---
+++ app/src/main/java/com/ai2020lab/cafe/activity/RegisteActivity.java
@@ -0,0 +1,15 @@
+package com.ai2020lab.cafe.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ai2020lab.cafe.R;
+
+public class RegisteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registe);
+    }
+}
